03535e1088cc1c0aeae72b4467a8dcc9366f37ba
Saarth Chahal <gamesgalore62@gmail.com>
2023-09-13 23:14:47 +0530

    Completed Catalogue portion
    Functionality of maintaining catalogue of headphones, along with capability of displaying all headphones, displaying specific headphone by id and adding new headphone

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,6 +1,10 @@
 package main
 
-// create struct for Headphone
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type headphone struct {
 	ID    string  `json:"id"`
@@ -9,12 +13,46 @@ type headphone struct {
 	Price float64 `json:"price"`
 }
 
+func getHeadphones(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, headphones)
+}
 
-// headphones record to see headphones data
 var headphones = []headphone{
 	{ID: "1", Item: "Wireless Headphone", Brand: " Sennheiser", Price: 8990},
 	{ID: "2", Item: "Foldable Headphone", Brand: "Zebronics", Price: 783},
 	{ID: "3", Item: " Wired Headphone", Brand: "JBL", Price: 599},
 	{ID: "4", Item: "Bluetooth Headphone", Brand: "boAT", Price: 999},
 	{ID: "5", Item: "Noise Cancellation Headphones", Brand: "Sony", Price: 9990},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getHeadphones)
+	router.GET("/albums/:id", getHeadphoneByID)
+	router.POST("/albums", postHeadphones)
+
+	router.Run("localhost:8080")
+}
+
+func postHeadphones(c *gin.Context) {
+	var newHeadphone headphone
+
+	if err := c.BindJSON(&newHeadphone); err != nil {
+		return
+	}
+
+	headphones = append(headphones, newHeadphone)
+	c.IndentedJSON(http.StatusCreated, newHeadphone)
+}
+
+func getHeadphoneByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range headphones {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "headphone not found"})
 }
